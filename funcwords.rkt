2f63fb71d646cfc664b6cf9aed30bbf1cce4df30
Thais Ribeiro <tha.sribeiro@gmail.com>
2018-07-21 23:16:19 -0300

    OnGoing testes BuildG

--- funcwords.rkt
+++ funcwords.rkt
@@ -3,5 +3,8 @@
 (require graph)
 
 (provide GetFile)
+(provide BuildG)
 
 (define (GetFile file) #t)
+
+(define (BuildG l-palavras) #t)
--- unit-tests.rkt
+++ unit-tests.rkt
@@ -7,16 +7,31 @@
 (define lista-entrada (list "Cahorrinhos" "são" "fofinhos"))
 (define lista-entrada1 (list "Bom" "dia" "jovens"))
 
+(define lista-grafo (list "a" "b" "c"))
+
 (define e-suite
   (test-suite
     "Testando dados usados nos testes"
     (check-true  (list? lista-entrada))
     (check-true  (list? lista-entrada1))
     (test-case
-      "Testa GetFile, que recebe um arquivo e devolve uma lista"
+      "Testa GetFile, que recebe um arquivo e devolve uma lista com as palavras dele"
       (define (test-getFile)
         (equal? (GetFile "teste.txt") lista-entrada)
         (not equal? (GetFile "teste.txt") lista-entrada1))
       (check-true (test-getFile)))))
+
+(define graph-suite
+  (test-suite
+    "Testando dados usados nos testes"
+    (check-true  (list? lista-grafo))
+    ;;(check-true  (list? lista-entrada1))
+    (test-case
+      "Testa BuildG, que recebe uma lista de palavras e retorna um grafo"
+      (define (test-BuildG)
+        (graph? (BuildG lista-grafo))
+        ;;(not equal? (GetFile "teste.txt") lista-entrada1))
+      (check-true (test-getFile)))))
+
 
 (run-tests e-suite)
