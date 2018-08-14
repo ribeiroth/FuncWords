57c9b3089f88865f7b975ed8ea8e19dc3441a720
Thais <thais@socialminer.com>
2018-08-14 14:28:30 -0300

    Centrality - OnGoing

--- funcwords.rkt
+++ funcwords.rkt
@@ -10,4 +10,11 @@
 
 (define (BuildG l-palavras) #t)
 
-(define (Centrality nOGraph) #t)
+(define (Centrality nOGraph)
+  (define all-vertices (get-vertices nOGraph))
+  (define g-size (length all-vertices))
+  (define centrality-list '())
+  (define (create-pair v)
+    (cons centrality-list (cons v (length (get-neighbors nOGraph v)))))
+  (map (lambda (x) (create-pair x)) all-vertices)
+  centrality-list)
