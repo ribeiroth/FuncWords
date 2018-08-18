4af44ee597b79a70b16b3e493b9112c13c7a9277
Thais Ribeiro <tha.sribeiro@gmail.com>
2018-08-18 19:18:39 -0300

    Calcular centralidade de forma paralela

--- funcwords.rkt
+++ funcwords.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require graph)
+(require future-visualizer)
 
 (provide GetFile)
 (provide BuildG)
@@ -13,10 +14,23 @@
 (define (Centrality nOGraph)
   (define all-vertices (get-vertices nOGraph))
   (define g-size (length all-vertices))
-  (define centrality-list '())
-  (map (lambda (x) (cons centrality-list (cons x (/ (length (get-neighbors nOGraph x)) (- g-size 1))))) all-vertices))
+  (define j (Partition all-vertices))
+  (define future-list (map (lambda (x) (future (lambda () (calc-Centrality nOGraph g-size x)))) j))
+  (define centrality-list (map (lambda (x) (touch x)) future-list))
+  centrality-list)
 
 (define (Keywords ctlList)
   (sort  ctlList
         (lambda (x y) (> (cdr x) (cdr y)))))
-  
+
+;;Funções auxiliares
+(define (calc-Centrality nOGraph g-size all-vertices)
+  (map (lambda (x) (cons x (/ (length (get-neighbors nOGraph x)) (- g-size 1)))) all-vertices))
+
+(define (Partition cList)
+  (define f (processor-count))
+  (define part (/ (length cList) f))
+  (let aux ([o-list cList] [rest-list null] [n 0])
+    (if (= n f)
+        (cons o-list rest-list )
+        (aux (drop o-list (floor part)) (cons (take o-list (floor part)) rest-list) (add1 n))))) 
