3267f3f92a66b1d3e6db854091676953255f7e8a
Thais Ribeiro <tha.sribeiro@gmail.com>
2018-08-20 00:21:54 -0300

    Fix tratamento de erro

--- main.rkt
+++ main.rkt
@@ -6,7 +6,7 @@
 
 (define (main args)
   (cond [(< (vector-length args) 3) (display "FuncWords: <arquivo-texto> <nome-arquivo-saida> <porcentagem de palavras chave desejada (Ex: 0.25)>\n" (current-error-port)) (exit 1)]
-        [else (null? (file->list (vector-ref args 0))) (display "Entre com um arquivo válido, por favor\n") current-error-port)) (exit 1)])
+        [else (null? (file->list (vector-ref args 0))) (display "Entre com um arquivo válido, por favor\n" (current-error-port)) (exit 1)])
          (define file-saida (vector-ref args 1))
          (define percent (vector-ref args 2))
          (define centrality-list (Centrality (file->list (vector-ref args 0))))
